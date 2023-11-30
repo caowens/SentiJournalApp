1512f06652a163830ff53c1a0b9a3f3a87356253
caowens <caowens555@yahoo.com>
2023-11-30 01:06:58 -0600

    Init firebase config

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAPiIA4GKM9FJo7-xPsQ9n5qhs2LK-b5j0",
+  authDomain: "sentijournal-fb-app.firebaseapp.com",
+  projectId: "sentijournal-fb-app",
+  storageBucket: "sentijournal-fb-app.appspot.com",
+  messagingSenderId: "215231695245",
+  appId: "1:215231695245:web:a373e214749eacf9e055c4",
+  measurementId: "G-SS83V9MHPJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
