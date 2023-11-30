5cac786bcfc6aed6561acb1883e4f383d74717de
caowens <caowens555@yahoo.com>
2023-11-30 16:11:48 -0600

    Make navbar solid white

--- src/components/ComplexNavbar.jsx
+++ src/components/ComplexNavbar.jsx
@@ -201,7 +201,7 @@ export function ComplexNavbar() {
   }, []);
  
   return (
-    <Navbar className="mx-auto max-w-screen-xl p-2 lg:rounded-full lg:pl-6 complex-nav-m">
+    <Navbar className="mx-auto max-w-screen-xl p-2 lg:rounded-full lg:pl-6 complex-nav-m complex-navbar">
       <div className="relative mx-auto flex items-center justify-between text-blue-gray-900">
         <Typography
           as="a"
