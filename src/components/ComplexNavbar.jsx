aa68cdd6885e7cba9d0b16734761b0021b550143
caowens <caowens555@yahoo.com>
2024-01-29 21:37:25 -0600

    Fix bug not showing login/signup forms because of requiring sign in before sign in

--- src/components/ComplexNavbar.jsx
+++ src/components/ComplexNavbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {
   Navbar,
@@ -30,179 +30,193 @@ import {
 } from "@heroicons/react/24/solid";
 import { auth } from "../firebase.js";
 
-const currentUser = auth.currentUser;
- 
-// profile menu component
-const profileMenuItems = [
-  {
-    label: "My Profile",
-    icon: UserCircleIcon,
-    route: "/signedin/" + currentUser.uid + "/profile",
-  },
-  {
-    label: "Edit Profile",
-    icon: Cog6ToothIcon,
-    route: "",
-  },
-  {
-    label: "Inbox",
-    icon: InboxArrowDownIcon,
-    route: "",
-  },
-  {
-    label: "Help",
-    icon: LifebuoyIcon,
-    route: "",
-  },
-  {
-    label: "Sign Out",
-    icon: PowerIcon,
-    route: "/",
-  },
-];
- 
-function ProfileMenu() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
- 
-  const closeMenu = () => setIsMenuOpen(false);
- 
-  return (
-    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
-      <MenuHandler>
-        <Button
-          variant="text"
-          color="blue-gray"
-          className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
-        >
-          <Avatar
-            variant="circular"
-            size="sm"
-            alt="tania andrew"
-            className="border border-gray-900 p-0.5"
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-          />
-          <ChevronDownIcon
-            strokeWidth={2.5}
-            className={`h-3 w-3 transition-transform ${
-              isMenuOpen ? "rotate-180" : ""
-            }`}
-          />
-        </Button>
-      </MenuHandler>
-      <MenuList className="p-1">
-        {profileMenuItems.map(({ label, icon, route }, key) => {
-          const isLastItem = key === profileMenuItems.length - 1;
-          return (
-            <Link to={route}>
-              <MenuItem
-                key={label}
-                onClick={closeMenu}
-                className={`flex items-center gap-2 rounded ${
-                  isLastItem
-                    ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
-                    : ""
-                }`}
-              >
-                {React.createElement(icon, {
-                  className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
-                  strokeWidth: 2,
-                })}
-                <Typography
-                  as="span"
-                  variant="small"
-                  className="font-normal"
-                  color={isLastItem ? "red" : "inherit"}
-                >
-                  {label}
-                </Typography>
-              </MenuItem>
-            </Link>
-          );
-        })}
-      </MenuList>
-    </Menu>
-  );
-}
- 
-// nav list menu
-const navListMenuItems = [
-  {
-    title: "@material-tailwind/html",
-    description:
-      "Learn how to use @material-tailwind/html, packed with rich components and widgets.",
-  },
-  {
-    title: "@material-tailwind/react",
-    description:
-      "Learn how to use @material-tailwind/react, packed with rich components for React.",
-  },
-  {
-    title: "Material Tailwind PRO",
-    description:
-      "A complete set of UI Elements for building faster websites in less time.",
-  },
-];
- 
-// nav list component
-const navListItems = [
-  {
-    label: "Entries",
-    icon: Square3Stack3DIcon,
-    route: "/signedin/" + currentUser.uid
-  },
-  {
-    label: "Account",
-    icon: UserCircleIcon,
-    route: "/signedin/" + currentUser.uid + "/profile"
-  },
-  {
-    label: "Analysis",
-    icon: ChartBarIcon,
-    route: "/signedin/" + currentUser.uid + "/analysis"
-  },
-  {
-    label: "New",
-    icon: PlusCircleIcon,
-    route: "/signedin/" + currentUser.uid + "/new"
-  },
-];
- 
-function NavList() {
-  return (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center">
-      {navListItems.map(({ label, icon, route }, key) => (
-        <Typography
-          key={label}
-          as="a"
-          href="#"
-          variant="small"
-          color="gray"
-          className="font-medium text-blue-gray-500"
-        >
-          <Link to={route}>
-          <MenuItem className="flex items-center gap-2 lg:rounded-full">
-              {React.createElement(icon, { className: "h-[18px] w-[18px]" })}{" "}
-              <span className="text-gray-900"> {label}</span>
-          </MenuItem>
-          </Link>
-        </Typography>
-      ))}
-    </ul>
-  );
-}
- 
 export function ComplexNavbar() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isNavOpen, setIsNavOpen] = React.useState(false);
- 
+
   const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
- 
+
   React.useEffect(() => {
     window.addEventListener(
       "resize",
-      () => window.innerWidth >= 960 && setIsNavOpen(false),
+      () => window.innerWidth >= 960 && setIsNavOpen(false)
     );
   }, []);
- 
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentUser(user);
+        setIsAuthenticated(true);
+      } else {
+        setCurrentUser(null);
+        setIsAuthenticated(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  // profile menu component
+  const profileMenuItems = [
+    {
+      label: "My Profile",
+      icon: UserCircleIcon,
+      route: isAuthenticated ? `/signedin/${currentUser.uid}/profile` : "",
+    },
+    {
+      label: "Edit Profile",
+      icon: Cog6ToothIcon,
+      route: "",
+    },
+    {
+      label: "Inbox",
+      icon: InboxArrowDownIcon,
+      route: "",
+    },
+    {
+      label: "Help",
+      icon: LifebuoyIcon,
+      route: "",
+    },
+    {
+      label: "Sign Out",
+      icon: PowerIcon,
+      route: "/",
+    },
+  ];
+
+  function ProfileMenu() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const closeMenu = () => setIsMenuOpen(false);
+
+    return (
+      <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+        <MenuHandler>
+          <Button
+            variant="text"
+            color="blue-gray"
+            className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
+          >
+            <Avatar
+              variant="circular"
+              size="sm"
+              alt="tania andrew"
+              className="border border-gray-900 p-0.5"
+              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+            />
+            <ChevronDownIcon
+              strokeWidth={2.5}
+              className={`h-3 w-3 transition-transform ${
+                isMenuOpen ? "rotate-180" : ""
+              }`}
+            />
+          </Button>
+        </MenuHandler>
+        <MenuList className="p-1">
+          {profileMenuItems.map(({ label, icon, route }, key) => {
+            const isLastItem = key === profileMenuItems.length - 1;
+            return (
+              <Link to={route}>
+                <MenuItem
+                  key={label}
+                  onClick={closeMenu}
+                  className={`flex items-center gap-2 rounded ${
+                    isLastItem
+                      ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+                      : ""
+                  }`}
+                >
+                  {React.createElement(icon, {
+                    className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
+                    strokeWidth: 2,
+                  })}
+                  <Typography
+                    as="span"
+                    variant="small"
+                    className="font-normal"
+                    color={isLastItem ? "red" : "inherit"}
+                  >
+                    {label}
+                  </Typography>
+                </MenuItem>
+              </Link>
+            );
+          })}
+        </MenuList>
+      </Menu>
+    );
+  }
+
+  // nav list menu
+  const navListMenuItems = [
+    {
+      title: "@material-tailwind/html",
+      description:
+        "Learn how to use @material-tailwind/html, packed with rich components and widgets.",
+    },
+    {
+      title: "@material-tailwind/react",
+      description:
+        "Learn how to use @material-tailwind/react, packed with rich components for React.",
+    },
+    {
+      title: "Material Tailwind PRO",
+      description:
+        "A complete set of UI Elements for building faster websites in less time.",
+    },
+  ];
+
+  // nav list component
+  const navListItems = [
+    {
+      label: "Entries",
+      icon: Square3Stack3DIcon,
+      route: isAuthenticated ? `/signedin/${currentUser.uid}` : "",
+    },
+    {
+      label: "Account",
+      icon: UserCircleIcon,
+      route: isAuthenticated ? `/signedin/${currentUser.uid}/profile` : "",
+    },
+    {
+      label: "Analysis",
+      icon: ChartBarIcon,
+      route: isAuthenticated ? `/signedin/${currentUser.uid}/analysis` : "",
+    },
+    {
+      label: "New",
+      icon: PlusCircleIcon,
+      route: isAuthenticated ? `/signedin/${currentUser.uid}/new` : "",
+    },
+  ];
+
+  function NavList() {
+    return (
+      <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center">
+        {navListItems.map(({ label, icon, route }, key) => (
+          <Typography
+            key={label}
+            as="a"
+            href="#"
+            variant="small"
+            color="gray"
+            className="font-medium text-blue-gray-500"
+          >
+            <Link to={route}>
+              <MenuItem className="flex items-center gap-2 lg:rounded-full">
+                {React.createElement(icon, { className: "h-[18px] w-[18px]" })}{" "}
+                <span className="text-gray-900"> {label}</span>
+              </MenuItem>
+            </Link>
+          </Typography>
+        ))}
+      </ul>
+    );
+  }
+
   return (
     <Navbar className="mx-auto max-w-screen-xl p-2 lg:rounded-full lg:pl-6 complex-nav-m complex-navbar">
       <div className="relative mx-auto flex items-center justify-between text-blue-gray-900">
@@ -211,9 +225,7 @@ export function ComplexNavbar() {
           href="#"
           className="mr-4 ml-2 cursor-pointer py-1.5 font-medium"
         >
-          <Link to={"/signedin/" + currentUser.uid}>
-            SentiJournal
-          </Link>
+          <Link to={isAuthenticated ? `/signedin/${currentUser.uid}` : "/"}>SentiJournal</Link>
         </Typography>
         <div className="hidden lg:block">
           <NavList />
@@ -227,7 +239,7 @@ export function ComplexNavbar() {
         >
           <Bars2Icon className="h-6 w-6" />
         </IconButton>
- 
+
         <ProfileMenu />
       </div>
       <MobileNav open={isNavOpen} className="overflow-scroll">
