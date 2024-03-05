f5b98acc1fe94818ff383019d31fda6027c17e35
caowens <caowens555@yahoo.com>
2024-03-04 21:36:00 -0600

    Add section describing the analysis

--- src/pages/Analysis.jsx
+++ src/pages/Analysis.jsx
@@ -133,37 +133,141 @@ export default function Analysis() {
   }, []);
 
   return (
-    <div className="chart-spacing">
-      <Card>
-        <CardHeader
-          floated={false}
-          shadow={false}
-          color="transparent"
-          className="flex flex-col gap-4 rounded-none md:flex-row md:items-center"
-        >
-          <div className="w-max rounded-lg bg-gray-900 p-5 text-white">
-            <ChartBarIcon className="h-6 w-6" />
-          </div>
-          <div>
+    <div className="analysis-container">
+      <div className="chart-spacing">
+        <Card>
+          <CardHeader
+            floated={false}
+            shadow={false}
+            color="transparent"
+            className="flex flex-col gap-4 rounded-none md:flex-row md:items-center"
+          >
+            <div className="w-max rounded-lg bg-gray-900 p-5 text-white">
+              <ChartBarIcon className="h-6 w-6" />
+            </div>
+            <div>
+              <Typography variant="h6" color="blue-gray">
+                Sentiment Analysis Line Chart
+              </Typography>
+              <Typography
+                variant="small"
+                color="gray"
+                className="max-w-sm font-normal"
+              >
+                A visualization of the sentiments of your entries. A positive
+                score represents a positive sentiment. A negative score means a
+                negative sentiment. If the sentiment is closer to 0, then it is
+                a more neutral sentiment.
+              </Typography>
+            </div>
+          </CardHeader>
+          <CardBody className="px-2 pb-0">
+            <Chart {...chartConfig} />
+          </CardBody>
+        </Card>
+      </div>
+
+      <div className="about-analysis">
+        <Card className="mt-6 w-96 about-analysis-card">
+          <CardBody className="about-analysis-card-body">
+            <div className="w-max rounded-lg bg-gray-900 p-5 text-white about-analysis-icon">
+              <ChartBarIcon className="h-6 w-6" />
+            </div>
+            <Typography variant="h4" color="blue-gray" className="mb-2">
+              Tracking Sentiment Trends
+            </Typography>
+            <Typography>
+              The "Analysis" page in SentiJournal offers users a powerful tool
+              to visualize the emotional trajectory of their journal entries
+              over time. Through the integration of advanced sentiment analysis
+              techniques, SentiJournal provides users with insights into the
+              sentiments expressed within their journal entries.
+            </Typography>
+            <br></br>
             <Typography variant="h6" color="blue-gray">
-              Sentiment Analysis Line Chart
+              Understanding the Sentiment Analysis Graph
             </Typography>
-            <Typography
-              variant="small"
-              color="gray"
-              className="max-w-sm font-normal"
-            >
-              A visualization of the sentiments of your entries. A positive
-              score represents a positive sentiment. A negative score means a
-              negative sentiment. If the sentiment is closer to 0, then it is a
-              more neutral sentiment.
+            <Typography>
+              <br></br>
+              The sentiment analysis graph presents a comprehensive overview of
+              the user's emotional journey as documented in their journal
+              entries. Here's how it works:
+              <br></br>
+              <br></br>
+              1. <b>Timeline Axis</b>: The horizontal axis represents time, with
+              each data point corresponding to a specific journal entry and its
+              associated timestamp. Users can navigate through their journal
+              entries chronologically, allowing them to reflect on past
+              experiences and emotions.
+              <br></br>
+              <br></br>
+              2. <b>Sentiment Score Axis</b>: The vertical axis quantifies the
+              sentiment score of each journal entry, derived through the&nbsp;
+              <a
+                href="https://huggingface.co/distilbert/distilbert-base-uncased-finetuned-sst-2-english?text=I+don%27t+like+you.+I+hate+you"
+                target="_blank"
+              >
+                DistilBERT-base-uncased text classification model, fine-tuned on
+                the SST-2 dataset
+              </a>
+              . The sentiment score provides a numerical representation of the
+              emotional tone expressed within the entry, ranging from highly
+              negative to highly positive.
+              <br></br>
+              <br></br>
+              3. <b>Line Graph Representation</b>: The sentiment analysis graph
+              is visualized as a line graph, with each point on the graph
+              representing the sentiment score of a particular journal entry.
+              The trendline connecting these points illustrates the overall
+              sentiment trajectory over time.
+              <br></br>
             </Typography>
-          </div>
-        </CardHeader>
-        <CardBody className="px-2 pb-0">
-          <Chart {...chartConfig} />
-        </CardBody>
-      </Card>
+            <br></br>
+            <Typography variant="h6" color="blue-gray">
+              Key Features and Benefits
+            </Typography>
+            <br></br>
+            <Typography>
+              <ul>
+                <li>
+                  <b>Insightful Reflection</b>: The sentiment analysis graph
+                  empowers users to reflect on their emotional experiences and
+                  identify recurring themes or significant events that influence
+                  their sentiments over time.
+                </li>
+                <br></br>
+                <li>
+                  <b>Tracking Emotional Well-being</b>: By tracking changes in
+                  sentiment trends, users can gain valuable insights into their
+                  emotional well-being, recognize patterns of positivity or
+                  negativity, and take proactive steps to maintain mental and
+                  emotional balance.
+                </li>
+                <br></br>
+                <li>
+                  <b>Goal Setting and Progress Monitoring</b>: SentiJournal
+                  enables users to set emotional well-being goals and monitor
+                  their progress through the sentiment analysis graph. Tracking
+                  improvements or setbacks in sentiment trends encourages users
+                  to cultivate positive habits and coping strategies.
+                </li>
+              </ul>
+            </Typography>
+            <br></br>
+            <Typography variant="h6" color="blue-gray">
+              Conclusion
+            </Typography>
+            <br></br>
+            <Typography>
+              The sentiment analysis graph by SentiJournal serves as a dynamic
+              tool for self-awareness, personal growth, and emotional
+              resilience. By harnessing the power of sentiment analysis, users
+              can navigate their emotional landscapes with clarity,
+              understanding, and intentionality.
+            </Typography>
+          </CardBody>
+        </Card>
+      </div>
     </div>
   );
 }
