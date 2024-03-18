ccb1018b22a18e2fadf6cbb9d54526fdb494b641
caowens <caowens555@yahoo.com>
2024-03-17 20:04:19 -0500

    Add main section

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -8,13 +8,41 @@ export function Landing() {
         <div className="header">
           SentiJournal
           <ul className="header-links">
-            <li className="about-us"><a href="">About Us</a></li>
-            <li className="contact"><a href="">Contact</a></li>
+            <li className="about-us">
+              <a href="">About Us</a>
+            </li>
+            <li className="contact">
+              <a href="">Contact</a>
+            </li>
           </ul>
           <ul className="header-buttons">
-            <Button variant="outlined" className="header-login"><Link to={"/login"}>Log in</Link></Button>
-            <Button className="header-signup"><Link to={"/signup"}>Sign Up</Link></Button>
+            <Button variant="outlined" className="header-login">
+              <Link to={"/login"}>Log in</Link>
+            </Button>
+            <Button className="header-signup">
+              <Link to={"/signup"}>Sign Up</Link>
+            </Button>
           </ul>
+        </div>
+
+        <div className="main-section section">
+          <div className="intro">
+            <div className="hero-main">
+              Discover your emotions with <br></br>{" "}
+              <h1 className="hero-main-title">SentiJournal</h1>
+            </div>
+            <div className="hero-secondary">
+              SentiJournal uses advanced sentiment analysis to track and
+              visualize your emotional journey through journaling. Gain insights
+              into your feelings and see how they evolve over time.
+            </div>
+            <div className="get-started-section">
+              <Button className="get-started-btn">Get Started</Button>
+              <Button variant="outlined" className="learn-more-btn">
+                <a href="">Learn More</a>
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </>
